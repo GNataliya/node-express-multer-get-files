83a4e461dc76f7f8079a5438d430573cb757989b
GNataliya <80199130+GNataliya@users.noreply.github.com>
2021-06-21 15:41:25 +0300

    level 2 with mistake 'err'

--- routes/picture.js
+++ routes/picture.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const path = require('path'); 
 const express = require('express');
 const multer = require('multer');
@@ -8,8 +9,19 @@ const upload = multer({
 });
 
 router.post('/', upload.single('some'), (req, res) => {
-    console.log(req.file);
+    //console.log(req.file);
+    //console.log(req.body);
+    const hesh = req.file.filename
+    const clientName = req.body.name;
+    console.log(hesh)
+    console.log(clientName)
+    const rename = fs.rename(hesh, clientName, (err) => {
+        if (err) throw err;
+        console.log('Rename complete!');
+      });
+
     res.json({ status: 'ok' });
 });
+
 
 module.exports = router;
